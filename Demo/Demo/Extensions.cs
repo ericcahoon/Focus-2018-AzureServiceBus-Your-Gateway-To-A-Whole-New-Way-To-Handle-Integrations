5f2c2c5854321c4f71e40d83cf3f8398df549d7d
Eric W. Cahoon <cahoon.eric@gmail.com>
2018-05-24 11:33:27 -0400

    Added extension  to serialize object to JSON, which makes output console window just a tad bit prettier

---
+++ Demo/Demo/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Demo
+{
+	internal static class Extensions
+	{
+		/// <summary>
+		/// Serializes the value to a JSON string.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value">The object to serialize to JSON</param>
+		/// <returns>JSON representation of the object</returns>
+		/// <exception cref="System.ArgumentNullException">text</exception>
+		public static string SerializeToJson<T>(this T value)
+		{
+			if (value == null)
+			{
+				throw new ArgumentNullException(nameof(value));
+			}
+
+			using (var memoryStream = new MemoryStream())
+			{
+				var serializer = new DataContractJsonSerializer(typeof(T));
+				serializer.WriteObject(memoryStream, value);
+				return Encoding.Default.GetString(memoryStream.ToArray());
+			}
+		}
+	}
+}
